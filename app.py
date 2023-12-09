5a7e78e9edefcaa2d52c96b3297ac2f28e64106d
Johnny <evgkisevgkis@gmail.com>
2023-12-09 14:19:12 +0500

    регистрация неймспейсов жанров и режиссеров

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_restx import Api
 
 from config import Config
-# from models import Review, Book
 from setup_db import db
 from views.movies import movie_ns
-# from views.reviews import review_ns
+from views.genres import genres_ns
+from views.directors import director_ns
 
 
 def create_app(config_object):
@@ -19,16 +19,18 @@ def register_extensions(app):
     db.init_app(app)
     api = Api(app)
     api.add_namespace(movie_ns)
+    api.add_namespace(director_ns)
+    api.add_namespace(genres_ns)
     create_data(app, db)
 
 
 def create_data(app, db):
-    # with app.app_context():
-    #     db.create_all()
-    #
-    #     with db.session.begin():
-    #         db.session.add_all()
-    pass
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+        # with db.session.begin():
+        #     db.session.add_all()
 
 
 app = create_app(Config())
