7b076bbfaf03cd3bc010cdc5e626404ce3f88f31
Johnny <evgkisevgkis@gmail.com>
2023-12-09 13:40:30 +0500

    написание представлений для жанров, методы GET

--- views/genres.py
+++ views/genres.py
@@ -1,15 +1,20 @@
 from flask_restx import Resource, Namespace
+from models import Genre, GenreSchema
+from setup_db import db
 
 genres_ns = Namespace('genres')
+genres_schema = GenreSchema()
 
 
 @genres_ns.route('/')
 class GenresView(Resource):
     def get(self):
-        return 'get', 200
+        genres = Genre.query.all()
+        return genres_schema.dump(genres, many=True), 200
 
 
 @genres_ns.route('/<int:gid>')
 class GenreView(Resource):
     def get(self, gid: int):
-        return 'get', 200
+        genre = Genre.query.get(gid)
+        return genres_schema.dump(genre), 200
