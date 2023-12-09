9786f8c53c84165291f8d1e08f0c29cb1086dcd9
Johnny <evgkisevgkis@gmail.com>
2023-12-09 13:29:56 +0500

    написание схем и моделей для фильмов, режиссеров и жанров

--- models.py
+++ models.py
@@ -1,11 +1,49 @@
-# здесь модель SQLAlchemy для сущности, также могут быть дополнительные методы работы с моделью (но не с базой)
+from setup_db import db
+from marshmallow import Schema, fields
 
-# Пример
-# from setup_db import db
-#
-# class Book(db.Model):
-#     __tablename__ = ‘book’
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
-#     author = db.Column(db.String)
-#     year = db.Column(db.Integer)
+
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+
+class DirectorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+
+class GenreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    trailer = db.Column(db.String)
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'))
+    genre = db.relationship('Genre')
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+    director = db.relationship('Director')
+
+
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre = fields.Str()
+    director = fields.Str()
