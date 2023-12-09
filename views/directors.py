02b57d455dede6b215be8e846e469ad9c4ad08cd
Johnny <evgkisevgkis@gmail.com>
2023-12-09 13:40:20 +0500

    написание представлений для режиссеров, методы GET

--- views/directors.py
+++ views/directors.py
@@ -1,15 +1,19 @@
 from flask_restx import Resource, Namespace
+from models import Director, DirectorSchema
 
 director_ns = Namespace('directors')
+directors_schema = DirectorSchema()
 
 
 @director_ns.route('/')
 class DirectorsView(Resource):
     def get(self):
-        return 'get', 200
+        directors = Director.query.all()
+        return directors_schema.dump(directors, many=True), 200
 
 
 @director_ns.route('/<int:did>')
 class DirectorView(Resource):
     def get(self, did: int):
-        return 'get', 200
+        director = Director.query.get(did)
+        return directors_schema.dump(director), 200
